5893f9020e5a73a257cfef2238115156019eaf2a
Sai Sashreek D <saisashreekd@gmail.com>
2024-08-01 10:42:41 +0530

    Added New Sections

--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
+import AboutSectionOne from "@/Components/About/AboutSectionOne";
+import AboutSectionTwo from "@/Components/About/AboutSectionTwo";
+import Contact from "@/Components/Contact";
 import Features from "@/Components/Features";
 import Hero from "@/Components/Hero";
+import Testimonials from "@/Components/Testimonials";
 
 export default function Home() {
   return (
     <>
      <Hero />
      <Features />
+     <AboutSectionOne />
+     <AboutSectionTwo />
+     <Testimonials />
+     <Contact />
     </>
   );
 }
