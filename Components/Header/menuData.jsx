55525d25710e215349075079a7566440d596c49a
Sai Sashreek D <saisashreekd@gmail.com>
2024-08-01 10:12:13 +0530

    Custom Menu Information

---
+++ Components/Header/menuData.jsx
@@ -0,0 +1,22 @@
+const menuData = [
+    {
+        id: 1,
+        title: "Home",
+        path: "/",
+        newTab: false
+    },
+    {
+        id: 2,
+        title: "About",
+        path: "/about",
+        newTab: false
+    },
+    {
+        id: 3,
+        title: "Contact",
+        path: "/contact",
+        newTab: false
+    }
+]
+
+export default menuData;
